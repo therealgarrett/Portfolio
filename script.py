98b1395ab9026312167a0856fac6c9e9e83d809f
Garrett Lubin <garrett@Garretts-MBP.attlocal.net>
2018-10-11 19:11:36 -0700

    #Debugged

--- script.py
+++ script.py
@@ -2,11 +2,32 @@ import random
 import math
 
 def main():
-    GAME_END_POINTS = 100
+    GAME_END_POINTS = 10
     COMPUTER_HOLD = 10
     is_user_turn = True
-    user_total = take_turn(True,COMPUTER_HOLD)
-    computer_total = take_turn(False,COMPUTER_HOLD)
+    print("\n")
+    print("Welcome to Jeopardy Dice!")
+    print("\n")
+    user_plcr = 0
+    computer_plcr = 0
+
+    while(user_plcr <= GAME_END_POINTS and computer_plcr <= GAME_END_POINTS):
+        print_current_player(is_user_turn)
+        user_total = take_turn(is_user_turn,COMPUTER_HOLD)
+        user_plcr = user_plcr + user_total
+        if user_total >= GAME_END_POINTS:
+            print("user wins!")
+            break
+        is_user_turn = get_next_player(is_user_turn)
+        print_current_player(is_user_turn)
+        computer_total = take_turn(is_user_turn,COMPUTER_HOLD)
+        computer_plcr = computer_plcr + computer_total
+        if computer_total >= GAME_END_POINTS:
+            print("computer wins!")
+            break
+        report_points(user_plcr,computer_plcr)
+        print("\n")
+        is_user_turn = get_next_player(is_user_turn)
 
 def print_current_player(is_user_turn):
     if is_user_turn == True:
@@ -22,25 +43,43 @@ def take_turn(is_user_turn, COMPUTER_HOLD):
     human_score = 0
     computer_score = 0
     if is_user_turn == True:
-        human_rand = roll_die()
+        begin = raw_input("roll? [yn]")
+        if begin == 'y' or begin == 'Y':
+            human_rand = roll_die()
+        elif begin == 'n' or begin == 'N':
+            is_user_turn = False
+            return human_score
         while human_rand != 1:
             human_score = human_score + human_rand
             if human_score != 0:
                 human_score = str(human_score)
+                human_rand = str(human_rand)
+                print("roll: " + human_rand)
                 print("Current Score: " + human_score)
+                human_rand = int(human_rand)
                 human_score = int(human_score)
                 again = raw_input("roll again? [yn]")
-                if again == 'y':
+                print("\n")
+                if again == 'y' or again == 'Y':
                     human_rand = roll_die()
                     continue
-                elif again == 'n':
-                    is_user_turn == False
+                elif again == 'n' or again == 'N':
+                    is_user_turn = False
                     break
             else:
                 human_score = 1
+                human_rand = str(human_rand)
+                print("roll: " + human_rand)
+                human_rand = int(human_rand)
                 print("You rolled a 1. Turn over.")
+                human_score = str(human_score)
+                print("Current Score: " + human_score)
+                human_score = int(human_score)
                 return human_score
         if human_rand == 1:
+            human_rand = str(human_rand)
+            print("roll: " + human_rand)
+            human_rand = int(human_rand)
             print("You rolled a 1. Turn over.")
             human_score = 1
             human_score = str(human_score)
@@ -54,20 +93,29 @@ def take_turn(is_user_turn, COMPUTER_HOLD):
         while computer_rand != 1:
             computer_score = computer_score + computer_rand
             if computer_score != 0:
+                computer_rand = str(computer_rand)
+                print("roll: " + computer_rand)
+                computer_rand = int(computer_rand)
                 if computer_score <= COMPUTER_HOLD:
                     computer_rand = roll_die()
                     continue
                 else:
                     computer_score = str(computer_score)
-                    print("*computer hold on " + computer_score + "*")
+                    print("*computer holds on " + computer_score + "*")
                     computer_score = int(computer_score)
-                    is_user_turn == True
+                    is_user_turn = True
                     break
             else:
                 computer_score = 1
+                computer_rand = str(computer_rand)
+                print("roll: " + computer_rand)
+                computer_rand = int(computer_rand)
                 print("The computer rolled a 1. Turn over.")
                 return computer_score
         if computer_rand == 1:
+            computer_rand = str(computer_rand)
+            print("rollclea " + computer_rand)
+            computer_rand = int(computer_rand)
             print("The computer rolled a 1. Turn over.")
             computer_score = 1
             return computer_score
@@ -75,12 +123,22 @@ def take_turn(is_user_turn, COMPUTER_HOLD):
             return computer_score
 
 def report_points(user_total,computer_total):
-    '''How can I call the take_turn function and continuously add up
-             turn totals until the 100 points mark is reached'''
+    print("\n")
+    print("The point totals are now: ")
+    user_total = str(user_total)
+    print("human: " + user_total)
+    user_total = int(user_total)
+    computer_total = str(computer_total)
+    print("computer: " + computer_total)
+    computer_total = int(computer_total)
 
-
-#print_current_player(False)
-#take_turn(False,10)
+def get_next_player(is_user_turn):
+    if is_user_turn == True:
+        is_user_turn = False
+        return is_user_turn
+    elif is_user_turn == False:
+        is_user_turn = True
+        return is_user_turn
 
 if __name__ == '__main__':
     main()
