2718709510b1939c727b45ed6d6f2f35691f1d36
Garrett Lubin <garrett@Garretts-MBP.attlocal.net>
2018-12-22 01:53:16 -0800

    Function restructuring and inquirer

--- PyTweets/script.py
+++ PyTweets/script.py
@@ -1,7 +1,9 @@
 import tweepy
+import sys
 import json
 import tweepy
 import pprint
+import inquirer
 import pymysql as db
 import pandas as pd
 import certifi
@@ -28,18 +30,13 @@ cursor = cnxt.cursor()
 
 
 class MyListener(tweepy.StreamListener):
-
     def on_connect(self):
         print(colored("Successfully connected to Twitter", "green"))
 
-    def on_error(self, status_code):
-        # On error - if an error occurs, display the error / status code
-        print(colored('An error has occured: ', 'red') + repr(status_code))
-        return False
-
     def __init__(self, api=None):
         self.num_tweets = 0
-        self.pbar = tqdm(total=100)
+        self.total_tweets = int(input("Number of tweets:"))
+        self.pbar = tqdm(total=self.total_tweets)
 
     def on_data(self, data):
         try:
@@ -61,7 +58,7 @@ class MyListener(tweepy.StreamListener):
         except BaseException as e:
             # Common error: Emoji tweets
             print(colored("Error on_data: %s", "red") % str(e))
-        if self.num_tweets < 100:
+        if self.num_tweets < self.total_tweets:
             return True
         else:
             self.pbar.close()
@@ -70,29 +67,11 @@ class MyListener(tweepy.StreamListener):
             return False
 
     def on_error(self, status):
-        print(status)
+        print('An Error has occured: ' + repr(status))
         return True
 
 
-def percentage(part, whole):
-    return 100 * float(part) / float(whole)
-
-
-def main():
-    consumer_key = 'y8js0cEdoYWFT5nQERuEm0fXI'
-    consumer_secret = 'DJdiZGtJSODiNQAGSDYhK3Lu4E69xiRHwf6x5nCffUW9Jq7fIm'
-    access_token = '32954643-EPgeI3VYUdojmCjZnxQ5XFsBl5EHwhuGYgn4B856p'
-    access_secret = 'h0qTDbDK59pGlqtxDuPj6CA3BC6rKr3ewHqLOH4oIEoh4'
-    search = input("Company Name: ")
-    go = input("Stream? [yn]")
-    if (go == "y" or go == 'Y'):
-        auth = OAuthHandler(consumer_key, consumer_secret)
-        auth.set_access_token(access_token, access_secret)
-        api = tweepy.API(auth)
-        twitter_stream = Stream(auth, MyListener())
-        twitter_stream.filter(track=[search])
-    elif (go == "n" or go == "N"):
-        pass
+def process_data():
     main_cnxt = db.connect(user='root', password='u0bj8dsvs1n',
                            host='localhost',
                            database='sys',
@@ -101,21 +80,32 @@ def main():
     main_cursor.execute("SELECT screen_name, location, text FROM PyData ")
     table = main_cursor.fetchall()
     number_rows = main_cursor.execute("SELECT * FROM PyData ")
-    main_cnxt.close()
-    main_cursor.close()
 
     # Restruces data into pandas datafram for easy use
-    df = pd.DataFrame([[ij for ij in i] for i in table])
-    df.rename(columns={0: 'Username', 1: 'Location', 2: 'Text'}, inplace=True)
+    processed = pd.DataFrame([[ij for ij in i] for i in table])
+    processed.rename(
+        columns={0: 'Username', 1: 'Location', 2: 'Text'}, inplace=True)
+    return processed, number_rows
 
+
+def data_size():
+    processed = process_data()
+    print("Tweets: " + str(processed[1]))
+
+
+def percentage(part, whole):
+    return 100 * float(part) / float(whole)
+
+
+def pie_chart(df_P):
     # Create init pie chart
     positive = 0
     negative = 0
     neutral = 0
     polarity = 0
-    num_terms = len(df)
-    for i in range(len(df)):
-        analysis = TextBlob(df['Text'][i])
+    num_terms = len(df_P)
+    for i in range(len(df_P)):
+        analysis = TextBlob(df_P['Text'][i])
         polarity += analysis.sentiment.polarity
         if(analysis.sentiment.polarity > 0.00):
             positive += 1
@@ -126,8 +116,8 @@ def main():
     positive = percentage(positive, num_terms)
     negative = percentage(negative, num_terms)
     neutral = percentage(neutral, num_terms)
-    labels = ['Positive [' + str(positive) + '%]', 'Neutral [' + str(neutral) + '%]',
-              'Negative [' + str(negative) + '%]']
+    labels = ['Positive [' + str(positive) + '%]', 'Negative [' + str(negative) + '%]',
+              'Neutral [' + str(neutral) + '%]']
     sizes = [positive, negative, neutral]
     colors = ['darkgreen', 'gold', 'red']
     patches, texts = plt.pie(sizes, colors=colors, startangle=90)
@@ -143,6 +133,8 @@ def main():
     elif(pie_promt == 'n' or pie_promt == 'N'):
         pass
 
+
+def heat_map(df_H1, df_H2):
     # Create heat map
     heat_promt = input("Load heat map? [yn]")
     if heat_promt == 'y' or heat_promt == 'Y':
@@ -153,8 +145,8 @@ def main():
         geolocator = Nominatim(timeout=10)
         coordinates = {'latitude': [], 'longitude': []}
 
-        for i in range(number_rows):
-            location = geolocator.geocode(df["Location"][i])
+        for i in range(df_H2):
+            location = geolocator.geocode(df_H1["Location"][i])
             if location:
                 coordinates['latitude'].append(location.latitude)
                 coordinates['longitude'].append(location.longitude)
@@ -164,13 +156,98 @@ def main():
         gmap.draw("heatmap.html")
     elif(heat_promt == 'n' or heat_promt == 'N'):
         pass
-    """
-    Note: Getting progress bar for gmap.draw
 
-    with open("gmplot/gmplot.py","r") as f:
-        print("n")
-        
-    """
+
+def v_menu(data_main):
+    questions = [
+        inquirer.List('V_Menu',
+                      message="Which visualization do you need?",
+                      choices=['Pie Chart', 'Heat Map', 'More Tweets', 'Quit'],
+                      ),
+    ]
+    answers = inquirer.prompt(questions)
+    if answers["V_Menu"] == 'Pie Chart':
+        pie_chart(data_main[0])
+    elif answers["V_Menu"] == 'Heat Map':
+        heat_map(data_main[0], data_main[1])
+    elif answers["V_Menu"] == 'More Tweets':
+        if __name__ == "__main__":
+            main()
+    if answers["V_Menu"] == 'Quit':
+        return
+
+
+def main_menu():
+    consumer_key = 'y8js0cEdoYWFT5nQERuEm0fXI'
+    consumer_secret = 'DJdiZGtJSODiNQAGSDYhK3Lu4E69xiRHwf6x5nCffUW9Jq7fIm'
+    access_token = '32954643-EPgeI3VYUdojmCjZnxQ5XFsBl5EHwhuGYgn4B856p'
+    access_secret = 'h0qTDbDK59pGlqtxDuPj6CA3BC6rKr3ewHqLOH4oIEoh4'
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_secret)
+    api = tweepy.API(auth)
+    questions = [
+        inquirer.List('Main Menu',
+                      message="Which operation do you need?",
+                      choices=['New Stream', 'Add Terms',
+                               'SQL Count', 'Visualization Menu', 'Quit'],),]
+    answers = inquirer.prompt(questions)
+    if answers["Main Menu"] == 'New Stream':
+        global words
+        words = []
+        term = input("Search Term: ")
+        words.append(term)
+        go = input("Stream? [yn]")
+        if (go == "y" or go == 'Y'):
+            twitter_stream = Stream(auth, MyListener())
+            twitter_stream.filter(track=words)
+            main_menu()
+        elif (go == "n" or go == "N"):
+            main_menu()
+
+    elif answers["Main Menu"] == 'Add Terms':
+        if 'words' in locals() or 'words' in globals():
+            pass
+        else:
+            words = []
+        try:
+            while(True):
+                add = input("Add Term? [yn]")
+                if(add == "y" or add == 'Y'):
+                    new_term = input("New Term: ")
+                    words.append(new_term)
+                    print("Current List: ", words)
+                    go = input("Stream? [yn]")
+                    if (go == "y" or go == 'Y'):
+                        twitter_stream = Stream(auth, MyListener())
+                        twitter_stream.filter(track=words)
+                        main_menu()
+                    elif (go == "n" or go == "N"):
+                        continue
+                elif(add == "n" or add == "N"):
+                    if words:
+                        print()
+                        print("Current List: ", words)
+                        print()
+                        return main_menu()
+        except BaseException as e:
+            print(colored("Error on_data: %s", "red") % str(e))
+            main_menu()
+
+    elif answers["Main Menu"] == 'SQL Count':
+        data_size()
+        print()
+        main_menu()
+
+    elif answers["Main Menu"] == 'Visualization Menu':
+        stream_df = process_data()
+        v_menu(stream_df)
+
+    elif answers["Main Menu"] == "Quit":
+        return
+
+
+def main():
+    main_menu()
 
 
 if __name__ == "__main__":
