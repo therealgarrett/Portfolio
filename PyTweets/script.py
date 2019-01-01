689a04911f45c3127adf0d5648cfce6a478c7aa8
Garrett Lubin <garrett@Garretts-MBP.attlocal.net>
2018-12-31 23:43:25 -0800

    added doc strings

--- PyTweets/script.py
+++ PyTweets/script.py
@@ -24,11 +24,29 @@ from folium.plugins import MarkerCluster
 from tqdm import tqdm
 
 
-class myListener(tweepy.StreamListener):
+class MyListener(tweepy.StreamListener):
+
+    """
+    MyListerner collects and manages incoming data using Tweepy.
+    Here script.py access Tweepy's library to perform conective duties
+    with Twitter's API.
+    """
+
     def on_connect(self):
+
+        """
+        Notifies user that Tweepy successfully connected to Twitterself.
+        """
+
         print(colored("Successfully connected to Twitter", "green"))
 
     def __init__(self, api=None):
+
+        """
+        Sets and organizes defult values for variables exclusive to the
+        MyListerner class.
+        """
+
         self.num_tweets = 0
         self.total_tweets = int(input("Number of tweets:"))
         self.pbar = tqdm(total=self.total_tweets)
@@ -37,8 +55,14 @@ class myListener(tweepy.StreamListener):
 
     def on_data(self, data):
 
+        """
+        Here data is streamed, parsed into a JSON format, and then
+        delivered to MySQL for later use. Any errors in data will be
+        reported to the terminal.
+        """
+
         try:
-            portalA = creds()
+            portal_1 = creds()
             rawTweets = json.loads(data)
             self.num_tweets += 1
             self.pbar.update(1)
@@ -50,9 +74,9 @@ class myListener(tweepy.StreamListener):
                 location = rawTweets["user"]["location"]
                 followers_count = rawTweets["user"]["followers_count"]
                 query = "INSERT INTO PyData (screen_name, created_at, location, followers_count, text) VALUES (%s,%s,%s,%s,%s)"
-                portalA[1].execute(
+                portal_1[1].execute(
                     query, (screen_name, created_at, location, followers_count, text))
-                portalA[0].commit()
+                portal_1[0].commit()
         except BaseException as e:
             # Ignoring emoji tweets for clean output
             if "Incorrect string value" in str(e):
@@ -67,18 +91,29 @@ class myListener(tweepy.StreamListener):
             self.pbar.close()
             print(colored("\n" + "Unsaved Total: " + str(self.unsaved), "green"))
             if self.emojis > 0:
-                print(colored("Contained Emojis: "
-                              + str(self.emojis) + "\n", "green"))
+                print(colored("Contained Emojis: " +
+                              str(self.emojis) + "\n", "green"))
             else:
                 "\n"
             return False
 
     def on_error(self, status):
+
+        """
+        This function reports an errors on Tweepy's connection
+        """
+
         print("An error has occured: " + repr(status))
         return True
 
 
 def creds():
+
+    """
+    The function connects the user to MySQl and opens a new cursor.
+    The user's log in credentials are required.
+    """
+
     cnxt = db.connect(user="root", password="u0bj8dsvs1n",
                       host="localhost",
                       database="sys",
@@ -87,12 +122,18 @@ def creds():
     return cnxt, cursor
 
 
-def deleteData():
+def delete_data():
+
+    """
+    This function clears MySQL of all data. Usefull for testing
+    results in Sentimap().
+    """
+
     ask = input("Reset PyData? [yn]")
     if ask == "y" or ask == "Y":
         try:
-            portalB = creds()
-            cursor = portalB[0].cursor()
+            portal_2 = creds()
+            cursor = portal_2[0].cursor()
             delete = "truncate PyData"
             cursor.execute(delete)
             remainder = cursor.execute("SELECT * FROM PyData ")
@@ -100,12 +141,17 @@ def deleteData():
         except BaseException as e:
             print(colored("Error on_data: %s", "red") % str(e))
     elif ask == "n" or ask == "N":
-        mainMenu()
+        main_menu()
 
 
-def processData():
-    portalC = creds()
-    cursor = portalC[0].cursor()
+def process_data():
+
+    """
+    This function creates a dataframe with all relevant Twitter data.
+    """
+
+    portal_3 = creds()
+    cursor = portal_3[0].cursor()
     cursor.execute(
         "SELECT screen_name, created_at, location, followers_count, text FROM PyData ORDER BY created_at DESC")
     rows = cursor.fetchall()
@@ -116,24 +162,39 @@ def processData():
     return processed
 
 
-def getDataSize():
-    processed = processData()
+def data_size():
+
+    """
+    This function prints out the number of rows in MySQL.
+    """
+
+    processed = process_data()
     print("Tweets: " + str(len(processed)))
 
 
 def percentage(part, whole):
+
+    """
+    This functions calculates the percentage of sentiments in
+    data set.
+    """
+
     return 100 * float(part) / float(whole)
 
 
-def pieChart(pieData):
-    # Create init pie chart
+def pie_chart(pie_data):
+
+    """
+    This function creates a pie chart with sentiment percentages.
+    """
+
     positive = 0
     negative = 0
     neutral = 0
     polarity = 0
-    pieTerms = len(pieData)
-    for i in range(len(pieData)):
-        analysis = TextBlob(pieData["Text"][i])
+    pie_count = len(pie_data)
+    for i in range(pie_count):
+        analysis = TextBlob(pie_data["Text"][i])
         polarity += analysis.sentiment.polarity
         if(analysis.sentiment.polarity > 0.00):
             positive += 1
@@ -141,9 +202,9 @@ def pieChart(pieData):
             negative += 1
         elif(analysis.sentiment.polarity == 0):
             neutral += 1
-    positive = percentage(positive, pieTerms)
-    negative = percentage(negative, pieTerms)
-    neutral = percentage(neutral, pieTerms)
+    positive = percentage(positive, pie_count)
+    negative = percentage(negative, pie_count)
+    neutral = percentage(neutral, pie_count)
     labels = ["Positive [" + str(round(positive)) + "%]", "Negative [" + str(round(negative)) + "%]",
               "Neutral [" + str(round(neutral)) + "%]"]
     sizes = [positive, negative, neutral]
@@ -160,79 +221,88 @@ def pieChart(pieData):
     os.execl(sys.executable, os.path.abspath("script.py"), *sys.argv)
 
 
-def heatMap(heatData):
-    polList, folList, folCountList, polCountList, pol_tots = [], [], [], [], []
-    polTotA, polTotB, polTotC, polTotD, polTotE = 0, 0, 0, 0, 0
-    folCountA, folCountB, folCountC, folCountD, folCountE = 0, 0, 0, 0, 0
-    polCountA, polCountB, polCountC, polCountD, polCountE = 0, 0, 0, 0, 0
-    for i in range(len(heatData)):
-        polarity = TextBlob(heatData["Text"][i]).sentiment.polarity
-        followers = heatData["Followers"][i]
+def heat_map(heat_data):
+
+    """
+    This function creates a heatmap. The data visualization shown,
+    presents the average polarity within each group. The y-axis contains
+    the number of users that fall within the inclusive followers range.
+    The x-axis contains the number of followers that fall within the inclusive
+    polarity range.
+    """
+
+    pol_list, fol_list, fol_count_list, pol_count_list, pol_tots = [], [], [], [], []
+    pol_tot_1, pol_tot_2, pol_tot_3, pol_tot_4, pol_tot_5 = 0, 0, 0, 0, 0
+    fol_count_1, fol_count_2, fol_count_3, fol_count_4, fol_count_5 = 0, 0, 0, 0, 0
+    pol_count_1, pol_count_2, pol_count_3, pol_count_4, pol_count_5 = 0, 0, 0, 0, 0
+    for i in range(len(heat_data)):
+        polarity = TextBlob(heat_data["Text"][i]).sentiment.polarity
+        followers = heat_data["Followers"][i]
         # create list of polarities and followers from each user
-        polList.append(polarity)
-        folList.append(followers)
-    for i in range(len(heatData)):
+        pol_list.append(polarity)
+        fol_list.append(followers)
+    for i in range(len(heat_data)):
         # update Follower counts
         # save polarities in each follower group for later averaging
-        if folList[i] < 10**1 and folList[i] >= 0:
-            folCountA += 1
-            polTotA += round(TextBlob(heatData["Text"]
-                                      [i]).sentiment.polarity, 2)
+        if fol_list[i] < 10**1 and fol_list[i] >= 0:
+            fol_count_1 += 1
+            pol_tot_1 += round(TextBlob(heat_data["Text"]
+                                        [i]).sentiment.polarity, 2)
 
-        elif folList[i] < 10**2 and folList[i] >= 10**1:
-            folCountB += 1
-            polTotB += round(TextBlob(heatData["Text"]
-                                      [i]).sentiment.polarity, 2)
+        elif fol_list[i] < 10**2 and fol_list[i] >= 10**1:
+            fol_count_2 += 1
+            pol_tot_2 += round(TextBlob(heat_data["Text"]
+                                        [i]).sentiment.polarity, 2)
 
-        elif folList[i] < 10**3 and folList[i] >= 10**2:
-            folCountC += 1
-            polTotC += round(TextBlob(heatData["Text"]
-                                      [i]).sentiment.polarity, 2)
+        elif fol_list[i] < 10**3 and fol_list[i] >= 10**2:
+            fol_count_3 += 1
+            pol_tot_3 += round(TextBlob(heat_data["Text"]
+                                        [i]).sentiment.polarity, 2)
 
-        elif folList[i] < 10**4 and folList[i] >= 10**3:
-            folCountD += 1
-            polTotD += round(TextBlob(heatData["Text"]
-                                      [i]).sentiment.polarity, 2)
+        elif fol_list[i] < 10**4 and fol_list[i] >= 10**3:
+            fol_count_4 += 1
+            pol_tot_4 += round(TextBlob(heat_data["Text"]
+                                        [i]).sentiment.polarity, 2)
 
-        elif folList[i] >= 10**4:
-            folCountE += 1
-            polTotE += round(TextBlob(heatData["Text"]
-                                      [i]).sentiment.polarity, 2)
+        elif fol_list[i] >= 10**4:
+            fol_count_5 += 1
+            pol_tot_5 += round(TextBlob(heat_data["Text"]
+                                        [i]).sentiment.polarity, 2)
 
         # update Polarity counts
-        if polList[i] < -1.6:
-            polCountA += 1
+        if pol_list[i] < -1.6:
+            pol_count_1 += 1
 
-        elif polList[i] > -1.6 and polList[i] < -0.6:
-            polCountB += 1
+        elif pol_list[i] > -1.6 and pol_list[i] < -0.6:
+            pol_count_2 += 1
 
-        elif polList[i] > -0.6 and polList[i] < 0.6:
-            polCountC += 1
+        elif pol_list[i] > -0.6 and pol_list[i] < 0.6:
+            pol_count_3 += 1
 
-        elif polList[i] > 0.6 and polList[i] < 1.6:
-            polCountD += 1
+        elif pol_list[i] > 0.6 and pol_list[i] < 1.6:
+            pol_count_4 += 1
 
         else:
-            polCountE += 1
+            pol_count_5 += 1
     # create y axis data containing count of followers that fall in each interval
-    folCountList.extend(
-        [folCountA, folCountB, folCountC, folCountD, folCountE])
+    fol_count_list.extend(
+        [fol_count_1, fol_count_2, fol_count_3, fol_count_4, fol_count_5])
     # create x axis data containing count of polarities that fall in each interval
-    polCountList.extend(
-        [polCountA, polCountB, polCountC, polCountD, polCountE])
-    pol_tots.extend([round(polTotA, 2), round(polTotB, 2), round(
-        polTotC, 2), round(polTotD, 2), round(polTotE, 2)])
+    pol_count_list.extend(
+        [pol_count_1, pol_count_2, pol_count_3, pol_count_4, pol_count_5])
+    pol_tots.extend([round(pol_tot_1, 2), round(pol_tot_2, 2), round(
+        pol_tot_3, 2), round(pol_tot_4, 2), round(pol_tot_5, 2)])
     r1, r2, r3, r4, r5 = [], [], [], [], []
     rows = [r1, r2, r3, r4, r5]
     # create each row with calculated averages
     for i in range(len(rows)):
-        for j in polCountList:
+        for j in pol_count_list:
             if j == 0:
                 rows[i].append(0.0)
             else:
                 rows[i].append(round((pol_tots[i] / j), 3))
 
-    df = pd.DataFrame(rows, folCountList, polCountList)
+    df = pd.DataFrame(rows, fol_count_list, pol_count_list)
     sns.heatmap(df, annot=True, fmt="g", cmap='viridis')
     plt.title("Average Sentiment Polarity by Group")
     plt.ylabel("hello")
@@ -242,9 +312,18 @@ def heatMap(heatData):
     plt.show()
 
 
-def sentiMap(sentiData):
-    portalD = creds()
-    cursor = portalD[0].cursor()
+def senti_map(senti_data):
+
+    """
+    This function creates a map containing all tweets from
+    the users database. Each marker on the map points to the
+    the locations from where the tweet was created. Also, each
+    marker receives the username, tweet, timestamp, and color
+    corresponding to the tweet's sentiment value.
+    """
+
+    portal_4 = creds()
+    cursor = portal_4[0].cursor()
     count = cursor.execute("SELECT * FROM PyData ")
     # Create heat map
     print(colored("Loading senti map ..", "green"))
@@ -260,37 +339,42 @@ def sentiMap(sentiData):
         mapvar = folium.Map(location=[40, -40],
 
                             zoom_start=3, tiles="Mapbox Control Room")
-        cordsCount = 0
+        cords = 0
         sentibar = tqdm(total=count)
         cluster = folium.plugins.MarkerCluster().add_to(mapvar)
         for i in range(count):
-            location = geolocator.geocode(sentiData["Location"][i])
+            location = geolocator.geocode(senti_data["Location"][i])
             if location:
-                polarity = TextBlob(sentiData["Text"][i]).sentiment.polarity
+                polarity = TextBlob(senti_data["Text"][i]).sentiment.polarity
                 if polarity > 0.0:
                     econ = folium.Icon(color="green")
                 elif polarity < 0.0:
                     econ = folium.Icon(color="red")
                 elif polarity == 0.0:
                     econ = folium.Icon(color="orange")
-                folium.Marker(popup="@" + sentiData["Username"][i] + ": " + sentiData["Text"][i] +
-                              " Followers: " +
-                              str(sentiData["Followers"][i]) +
-                              " Created at: " +
-                              str(sentiData["Created at"][i]),
+                folium.Marker(popup="@" + senti_data["Username"][i] + ": " + senti_data["Text"][i] +
+                              " Followers: "
+                              + str(senti_data["Followers"][i])
+                              + " Created at: "
+                              + str(senti_data["Created at"][i]),
                               location=[location.latitude, location.longitude], icon=econ).add_to(cluster)
-            cordsCount += 1
+            cords += 1
             sentibar.update(1)
         sentibar.close()
         mapvar.save("sentimap.html")
         print(colored("Click markers for more details", "green"))
-        vMenu()
+        v_menu()
     except BaseException as e:
         print(colored("Geocoder Error: %s", "red") % str(e))
 
 
-def vMenu():
-    vizData = processData()
+def v_menu():
+
+    """
+    Generates a menu of available data visualizations
+    """
+
+    v_data = process_data()
     questions = [
         inquirer.List("visuals",
                       message="Which visualization do you need?",
@@ -300,11 +384,11 @@ def vMenu():
     ]
     answers = inquirer.prompt(questions)
     if answers["visuals"] == "Pie Chart":
-        pieChart(vizData)
+        pie_chart(v_data)
     elif answers["visuals"] == "Senti Map":
-        sentiMap(vizData)
+        senti_map(v_data)
     elif answers["visuals"] == "Heat Map":
-        heatMap(vizData)
+        heat_map(v_data)
     elif answers["visuals"] == "Back":
         if __name__ == "__main__":
             main()
@@ -312,7 +396,13 @@ def vMenu():
         sys.exit()
 
 
-def mainMenu():
+def main_menu():
+
+    """
+    This function authorizes the use of Twitter's API and allows
+    the user to call functions from a menu.
+    """
+
     consumerKey = "y8js0cEdoYWFT5nQERuEm0fXI"
     consumerSecret = "DJdiZGtJSODiNQAGSDYhK3Lu4E69xiRHwf6x5nCffUW9Jq7fIm"
     accessToken = "32954643-EPgeI3VYUdojmCjZnxQ5XFsBl5EHwhuGYgn4B856p"
@@ -335,11 +425,11 @@ def mainMenu():
         print("Current List: ", WORDS)
         go = input("Stream? [yn]")
         if (go == "y" or go == "Y"):
-            twitterStream = Stream(auth, myListener())
-            twitterStream.filter(track=WORDS)
-            mainMenu()
+            twitter_stream = Stream(auth, MyListener())
+            twitter_stream.filter(track=WORDS)
+            main_menu()
         elif (go == "n" or go == "N"):
-            mainMenu()
+            main_menu()
 
     elif answers["Main Menu"] == "Add Terms":
         if "WORDS" in locals() or "WORDS" in globals():
@@ -347,63 +437,68 @@ def mainMenu():
         else:
             WORDS = []
         while(True):
-            newTerm = input("New Term: ")
-            WORDS.append(newTerm)
+            new_term = input("New Term: ")
+            WORDS.append(new_term)
             print("Current List: ", WORDS)
             go = input("Stream? [yn]")
             if (go == "y" or go == "Y"):
-                twitterStream = Stream(auth, myListener())
-                twitterStream.filter(track=WORDS)
-                mainMenu()
+                twitter_stream = Stream(auth, MyListener())
+                twitter_stream.filter(track=WORDS)
+                main_menu()
             elif (go == "n" or go == "N"):
                 print()
                 print("Current List: ", WORDS)
                 print()
-                mainMenu()
+                main_menu()
     elif answers["Main Menu"] == "Stream":
         try:
             go = input("Stream? [yn]")
             if (go == "y" or go == "Y"):
-                twitterStream = Stream(auth, myListener())
-                twitterStream.filter(track=WORDS)
-                mainMenu()
+                twitter_stream = Stream(auth, MyListener())
+                twitter_stream.filter(track=WORDS)
+                main_menu()
             elif (go == "n" or go == "N"):
                 print()
                 print("Current List: ", WORDS)
                 print()
-                mainMenu()
+                main_menu()
         except BaseException as e:
             print(colored("Error on_data: %s", "red") % str(e))
-            mainMenu()
+            main_menu()
 
     elif answers["Main Menu"] == "Get Words":
         try:
             print()
             print("Current List: ", WORDS)
             print()
-            mainMenu()
+            main_menu()
         except BaseException as e:
             print(colored("Error on_data: %s", "red") % str(e))
-            mainMenu()
+            main_menu()
 
     elif answers["Main Menu"] == "SQL Count":
         print()
-        getDataSize()
+        data_size()
         print()
-        mainMenu()
+        main_menu()
 
     elif answers["Main Menu"] == "SQL Reset":
-        deleteData()
+        delete_data()
 
     elif answers["Main Menu"] == "Visualization Menu":
-        vMenu()
+        v_menu()
 
     elif answers["Main Menu"] == "Quit":
         sys.exit()
 
 
 def main():
-    mainMenu()
+
+    """
+    Calls main_menu()
+    """
+    
+    main_menu()
 
 
 if __name__ == "__main__":
